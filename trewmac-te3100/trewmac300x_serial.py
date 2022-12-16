01c6a32142a0058d20ad759f9cb9fec6bd414c01
LarsPrivat <lars.hoff@usn.no>
2022-12-16 11:08:50 +0100

    Update trewmac300x_serial.py

--- trewmac-te3100/trewmac300x_serial.py
+++ trewmac-te3100/trewmac300x_serial.py
@@ -15,31 +15,91 @@ Created on Mon Dec 12 11:36:32 2022
 
 import serial    # Uses serial communication (COM-ports)
 import numpy as np
+import time
 
 terminator=b'\r'
 
+class te_result:  # Initialise with impossible values. To be set at object creation
+    def __init__( self ):
+        self.fmin  = 0.0
+        self.fmax  = 0.0
+        self.np    = 0
+        
+        self.averaging =-1.0
+        self.z0        =-1.0
+        self.output    =-1.0
+        
+        self.format = 'undefined'
+        self.mode   = 'undefined'
+        self.baudrate = 0.0
+        
+        self.f     = np.array(0.0)
+        self.Zmag  = np.array(0.0)
+        self.Zphase= np.array(0.0)
+        
+        
 #%%
 class te300x:
-    def __init__( self, port='COM1', timeout=10 ):
-        self.port = serial.Serial( port, 115200, timeout = timeout )       
-        self.f    = np.array(0.0)
-        self.Zmag = np.array(0.0)
-        self.Zphi = np.array(0.0)
+    def __init__( self, port = 'COM1', timeout = 5 ):
+        self.port = serial.Serial( port, 115200, timeout = timeout )    
+        self.res  = te_result()
+        self.set_frequencyrange( fmin= 300e3, fmax= 20e6, np= 500 )
+        self.set_averaging ( avg = 16 )
+        self.set_z0 ( z0 = 50 )
+        self.set_output ( output = 100 )
+        self.set_format( dataformat = 'polZ' )
+        self.set_mode ( mode = 'T' )
+        # self.set_baudrate( baud = 115200 )
 
-    def read_text( self, max_length = 100 ):
+    def close(self):
+        self.port.close()
+        return 0      
+
+    # Utility functions
+    def read_text( self, max_length = 1000 ):
         rep = self.port.read_until( expected= terminator, size= max_length )
-        return rep.removesuffix( b'\r' ).decode()
+        return rep.removesuffix( terminator ).decode()
         
-    def read_values( self, max_length = 100 ):
+    def read_values( self, max_length = 1000 ):
         rep = self.port.read_until( expected= terminator, size= max_length )
         rep = rep.split(b',')
         rep = list(map(float, rep))
         return rep
-        
-    def close(self):
-        self.port.close()
-        return 0      
-        
+    
+    def read_sweep_values(self):     
+        finished = False
+        val=b''
+        while not(finished):  # Read multiple times until all data acquired
+            val = val + self.port.read_until( expected = b'END\r' )   
+            finished = (self.port.in_waiting == 0)
+        return val.decode()
+    
+    def read_sweep_line(self):
+        f   = Zmag = Zphi = 0
+        val = self.read_text()           
+        finished = (val=='END') 
+        if not(finished):
+            line= val.split(',')
+            f    = float( line[0] )
+            Zmag = float( line[1] )
+            Zphi = float( line[2] )
+            
+        return [f, Zmag, Zphi, finished ]
+
+                               
+    def send_configure ( self, parameter, value ):
+        command = f'C{parameter}'
+        fullcommand =  command.encode() + terminator + value.encode() + terminator
+        self.port.write( fullcommand )
+        return self.read_text()
+
+    def send_freqrange ( self, parameter, value ):
+        fullcommand =  parameter.encode() + value.encode() + terminator
+        self.port.write( fullcommand )
+        response = self.read_text()
+        return float( response.split('=')[1] )
+
+    # Read device information   
     def read_version(self):
         self.port.write(b'V')
         return self.read_text()
@@ -48,82 +108,110 @@ class te300x:
         self.port.write(b'I')
         return self.read_text()
     
+    # Configure device    
     def set_frequencyrange( self, fmin= 300e3, fmax= 20e6, np= 801 ):
-        fmin_command = f'S{fmin/1e6:.2f}\r'.encode() 
-        fmax_command = f'S{fmax/1e6:.2f}\r'.encode() 
-        np_command   = f'P{np:d}\r'.encode() 
-        self.port.write( fmin_command  )
-        self.port.write( fmax_command )
-        self.port.write( np_command )
+        self.res.fmin = self.send_freqrange ( 'S', f'{fmin/1e6:.2f}'  )
+        self.res.fmax = self.send_freqrange ( 'E', f'{fmax/1e6:.2f}'  )
+        np            = self.send_freqrange ( 'P', f'{np:d}'  ) 
+        self.res.np   = int (np )                             
         return 0
     
-    def read_sweep( self ):
-        self.port.write(b'I')
-        f   = []
-        Zmag= []
-        Zphi= []        
-        val = self.port.read_until( expected=b'END\r', size=200 )   
-        val = val.split(b'\r')
-        for k in range( 1, len(val)-1 ):
-            line=val[k].split(b',')
-            f.append(float( line[0] ))
-            Zmag.append(float( line[1] ))
-            Zphi.append(float( line[2] ))
+    def set_format( self, dataformat = 'polZ' ):
+        result = self.send_configure ( 'format', dataformat )      
+        self.res.format = result.split('=')[1] 
+        return self.res.format 
+    
+    def set_averaging ( self, avg = 64 ):         
+        result = self.send_configure ( 'averaging', f'{avg:d}' )     
+        self.res.averaging = int( result.split('=')[1] )
+        return self.res.averaging
 
-        self.f     = np.array(f)
-        self.Zmag  = np.array(Zmag)
-        self.Zphase= np.array(Zphi)
-        return 0
+    def set_output ( self, output = 100 ):  
+        result = self.send_configure ( 'output', f'{output:d}' )      
+        value  = result.split('=')[1]
+        self.res.output  = float( value.split('%')[0] )
+        return self.res.output  
+    
+    def set_z0 ( self, z0 = 50 ):  
+        result = self.send_configure ( 'zo', f'{z0:0.1f}' )    
+        self.res.z0  = float( result.split('=')[1] )
+        return self.res.z0
+    
+    def set_mode ( self, mode = 'T' ):  
+        if mode.lower()[0] == 't':
+            value = 'S11'
+        else:
+            value= 'S21'
+        result = self.send_configure ( 'mode', value )      
+        self.res.mode  = result.split('=')[1]
+        return self.res.mode 
 
-
-# =============================================================================
-#     def read_sweep_log( self ):
-#         self.port.write(b'G')        
-#         return self.port.read_until( expected=b'END\r', size=200 )    
-# =============================================================================
-
+    def set_baudrate( self, baud = 115200 ):  
+        if baud > 10000:
+            baud = 115200
+        else:
+            baud = 9600        
+        result = self.send_configure( 'baud', f'{baud:d}' )      
+        self.res.baudrate  = result.split(' ')[2]
+        return self.res.baudrate            
+    
+    # Read results
     def read_single( self, freq ):
         f_command= f'F{freq/1e6:.2f}\r'.encode()
         self.port.write( f_command )
         rep = self.read_values()
-        self.f    = np.array( rep[0] )
-        self.Zmag = np.array( rep[1] )
-        self.Zphi = np.array( rep[2] )
+        self.res.f    = np.array( rep[0] )
+        self.res.Zmag = np.array( rep[1] )
+        self.res.Zphi = np.array( rep[2] )
         return rep
 
+    def read_sweep( self ):
+        f   = []
+        Zmag= []
+        Zphi= []   
+        self.port.reset_input_buffer()
+        self.port.write(b'N')
+        tic = time.perf_counter()
+        val = self.read_sweep_values()
+        val = val.split('\r')
+        nf  = len(val)-3 
+        for k in range( 1, nf+1 ):
+            line=val[k].split(',')
+            f.append(float( line[0] ))
+            Zmag.append(float( line[1] ))
+            Zphi.append(float( line[2] ))
 
-    def set_format( self, dataformat = 'polZ' ):
-        dataformat_command = f'{dataformat}\r'.encode()
-        self.port.write( dataformat_command )
-        return self.read_text()  
+        dt = time.perf_counter() - tic 
+        self.res.f     = np.array(f)
+        self.res.Zmag  = np.array(Zmag)
+        self.res.Zphase= np.array(Zphi)
+        self.res.nf    = nf
+        self.res.dt    = dt
+        return 0
+    
+    def read_sweep_point_by_point( self ):
+        f   = []
+        Zmag= []
+        Zphi= []   
+        self.port.write(b'N')
+        tic = time.perf_counter()
+        header   = self.read_text()
+        finished = False
+        nf       = 0
+        while not(finished):
+            ret= self.read_sweep_line()
+            finished = ret[3] or (nf > self.res.np)
+            if not(finished):
+                nf = nf+1                
+                f.append( ret[0] )
+                Zmag.append( ret[1] )
+                Zphi.append( ret[2] )
 
-
-    def set_averaging ( self, avg = 64 ):  # Check formats below in references
-        avg_command = f'Caveraging\r{avg:d}\r'.encode()
-        self.port.write( avg_command )
-        return self.read_text()
-
-
-    def set_output ( self, output = 100 ):  # Check formats below in references
-        output_command = f'Coutput{output:d}\r'.encode()
-        self.port.write( output_command )
-        return self.read_text()
-
-
-    def set_mode ( self, mode = 'T' ):  # Check formats below in references
-        if mode.lower()[0] == 't':
-            self.port.write(b'CmodeS11\r')
-        else:
-            self.port.write(b'CmodeS22\r')
-        return self.read_text()  
-
-
-    def set_baudrate( self, baud = 115200 ):  # Check formats below in references
-        if baud > 10000:
-            self.port.write(b'Cbaud115200\r')
-        else:
-            self.port.write(b'Cbaud9600\r')
-        return self.read_text()
-
-
-
+        dt = time.perf_counter() - tic    
+        self.res.f     = np.array(f)
+        self.res.Zmag  = np.array(Zmag)
+        self.res.Zphase= np.array(Zphi)
+        self.res.nf    = nf
+        self.res.dt    = dt
+        return 0
+    
